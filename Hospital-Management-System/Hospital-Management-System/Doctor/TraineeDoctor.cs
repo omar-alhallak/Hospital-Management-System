c98f3d2bc31fe9df7c828b5ac9c579e9c8b7ad3d
Omar alhallak <omaralhallak0998976714@gmail.com>
2026-03-08 23:57:02 +0300

    Build Architecture For App

---
+++ Hospital-Management-System/Hospital-Management-System/Doctor/TraineeDoctor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System.Doctor
+{
+    internal class TraineeDoctor
+    {
+    }
+}
---
+++ Hospital-Management-System/Hospital-Management-System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hospital_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
